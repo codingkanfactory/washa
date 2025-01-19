dc0c0da43174b86e3b1910b2cd49b4b217666b60
codingkanfactory <syauqi.oku.bta@gmail.com>
2025-01-20 06:14:34 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,37 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-        import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
 
-        const firebaseConfig = {
-            apiKey: "AIzaSyDMcax3DSMbwCdf6ilMStGr7-wL3GVf8-Q",
-            authDomain: "codingkan.firebaseapp.com",
-            databaseURL: "https://codingkan-default-rtdb.firebaseio.com",
-            projectId: "codingkan",
-            storageBucket: "codingkan.firebasestorage.app",
-            messagingSenderId: "753036437410",
-            appId: "1:753036437410:android:babfafdcad76fd566234fb"
-        };
+const firebaseConfig = {
+    apiKey: "AIzaSyDMcax3DSMbwCdf6ilMStGr7-wL3GVf8-Q",
+    authDomain: "codingkan.firebaseapp.com",
+    databaseURL: "https://codingkan-default-rtdb.firebaseio.com",
+    projectId: "codingkan",
+    storageBucket: "codingkan.firebasestorage.app",
+    messagingSenderId: "753036437410",
+    appId: "1:753036437410:android:babfafdcad76fd566234fb"
+};
 
-        const app = initializeApp(firebaseConfig);
-        const db = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-        const userForm = document.getElementById('userForm');
-        userForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            
-            const message = document.getElementById('message').value;
+const userForm = document.getElementById('userForm');
+const successDialog = document.getElementById('successDialog');
+const closeDialog = document.getElementById('closeDialog');
 
-            const messagesRef = ref(db, 'messages');
-            push(messagesRef, {
-                
-                message,
-                timestamp: new Date().toISOString()
-            });
+userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = document.getElementById('message').value;
 
-            alert('Pesan berhasil dikirim!');
-            userForm.reset();
-        });
+    const messagesRef = ref(db, 'messages');
+    push(messagesRef, {
+        message,
+        timestamp: new Date().toISOString()
+    });
+
+    successDialog.showModal();
+    userForm.reset();
+});
+
+closeDialog.addEventListener('click', () => {
+    successDialog.close();
+});
